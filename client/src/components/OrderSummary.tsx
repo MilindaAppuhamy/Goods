53b6a7992eb84cd61ab5c63638a2dc36416537de
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-12-11 15:15:03 +0000

    Order summary UI completed.

---
+++ client/src/components/OrderSummary.tsx
@@ -0,0 +1,64 @@
+import { Box, Container, Divider, Flex, Heading, Text } from "@chakra-ui/react";
+import { CartType } from "../pages/BasketPage";
+
+const OrderSummary = ({ userCart }: { userCart: CartType }) => {
+  const total = userCart
+    ?.map((cartItem) => cartItem?.item?.price! * cartItem?.count!)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  const formattedTotal = `£${(total / 100).toFixed(2)}`;
+
+  return (
+    <Box
+      gridArea={"summary"}
+      ml={{ md: 1, base: 0 }}
+      width={"98%"}
+      height={"max-content"}
+      backgroundColor={"white"}
+      borderRadius={"lg"}
+      p={{ md: 3, base: 2 }}
+    >
+      <Heading fontSize={"larger"} color={"#5D3FD3"} mb={30}>
+        Order summary
+      </Heading>
+      <Box>
+        {userCart?.map((cartItem) => (
+          <Container key={cartItem.id}>
+            <Flex justifyContent={"space-between"} my={3}>
+              <Box width={"60%"}>
+                <Text fontWeight={"medium"}>{cartItem.item?.name}</Text>
+                <Text fontSize={"medium"} color={"GrayText"}>
+                  x {cartItem.count}
+                </Text>
+              </Box>
+              <Text>{`£${(cartItem.item!?.price / 100).toFixed(2)}`}</Text>
+            </Flex>
+            <Divider />
+          </Container>
+        ))}
+      </Box>
+      <Box
+        height={"1px"}
+        width={"100%"}
+        backgroundColor={"black"}
+        borderRadius={3}
+        mt={4}
+        mb={3}
+      />
+      <Box
+        display={"flex"}
+        flexDir={"row"}
+        justifyContent={"space-around"}
+        mt={6}
+        mb={3}
+      >
+        <Text fontWeight={"bold"}>Total:</Text>
+        <Text fontWeight={"bold"} color={"#5D3FD3"}>
+          {formattedTotal}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default OrderSummary;
