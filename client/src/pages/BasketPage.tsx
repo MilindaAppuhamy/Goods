0d19e5c47a34c417eb9856dbb65d549b900f363e
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-12-11 15:13:30 +0000

    Basket page layout.

--- client/src/pages/BasketPage.tsx
+++ client/src/pages/BasketPage.tsx
@@ -1,10 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
+import Basket from "../components/Basket";
+import { ItemType } from "../components/ItemCard";
+import OrderSummary from "../components/OrderSummary";
+import useGetCart from "../hooks/useGetCart";
+import getUserHeaders from "../utils/getUserHeaders";
+
+export type CartItemType = {
+  id?: number;
+  itemId: number;
+  count?: number;
+  userId?: number | string;
+  item?: ItemType;
+};
+
+export type CartType = CartItemType[];
 
 const BasketPage = () => {
+  const headers = getUserHeaders();
+  const userId = JSON.parse(localStorage.getItem("userId")!);
+  const { data } = useGetCart(userId!, headers!);
+  const userCart: CartType = data?.data;
+
   return (
-    <>
-      <Box>basket page</Box>
-    </>
+    <SimpleGrid
+      gridTemplateAreas={{
+        md: `"basket summary"`,
+        base: `"summary" 
+              "basket"`,
+      }}
+      gridTemplateRows={{ md: "1fr", base: "auto" }}
+      gridTemplateColumns={{ md: "65% 35%", base: "1fr" }}
+      justifyItems={{ md: "center", base: "unset" }}
+      m={{ md: 2, base: 0 }}
+    >
+      <Basket userCart={userCart} />
+      <OrderSummary userCart={userCart} />
+    </SimpleGrid>
   );
 };
 
