d9a9e06b675985a305e7397055c855539ead445f
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-12-12 11:18:47 +0000

    user seed data added.

--- server/src/seed.ts
+++ server/src/seed.ts
@@ -2,8 +2,11 @@ import { Sequelize } from "sequelize";
 import { Item } from "./models/item";
 import { User } from "./models/user";
 import { CartItem } from "./models/cartItem";
+const bcrypt = require("bcrypt");
 
 const itemData = require("../../server/itemSeedData.json");
+const userData = require("../../server/userSeedData.json");
+
 const db: Sequelize = require("./db");
 
 const seed = async () => {
@@ -15,11 +18,35 @@ const seed = async () => {
   User.hasMany(CartItem);
   CartItem.belongsTo(User);
 
+  //Item - CartItem association
+  Item.hasOne(CartItem);
+  CartItem.belongsTo(Item);
+
   //starting db
   await db.sync();
 
   // add the data
-  await Item.bulkCreate(itemData);
+  const items = await Item.bulkCreate(itemData);
+  const users = (await User.bulkCreate(userData)) as any;
+
+  //hashing the passwords
+  for (let user of users) {
+    console.log(user.password);
+    const salt: string = await bcrypt.genSalt(6);
+    let hashedPassword = await bcrypt.hash(user.password, salt);
+    await user.update({ password: hashedPassword });
+    console.log(user.password);
+  }
+
+  //adding some items to users
+  Promise.all([
+    users[0].addItem(items[0]),
+    users[0].addItem(items[1]),
+    users[0].addItem(items[2]),
+    users[1].addItem(items[3]),
+    users[1].addItem(items[4]),
+    users[1].addItem(items[5]),
+  ]);
 
   console.log("db populated with initial data!");
 };
