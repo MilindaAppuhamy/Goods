4b2e597e9207cff63267e2ac818690cdf7978489
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-12-12 11:18:45 +0000

    Wishlist coming soon text added.

--- client/src/pages/WishlistPage.tsx
+++ client/src/pages/WishlistPage.tsx
@@ -3,7 +3,17 @@ import { Box } from "@chakra-ui/react";
 const WishlistPage = () => {
   return (
     <>
-      <Box>WishlistPage</Box>
+      <Box
+        width={"100%"}
+        height={"100%"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        fontWeight={"bold"}
+        fontSize={"larger"}
+      >
+        Coming soon...
+      </Box>
     </>
   );
 };
